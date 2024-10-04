e4badc9fb8a28876e0defad79102871acb1be999
jawor182 <kubajaworski59@outlook.com>
2024-10-04 22:33:49 +0200

    xinit

--- .config/nvim/lua/plugins/noice.lua
+++ .config/nvim/lua/plugins/noice.lua
@@ -11,6 +11,9 @@ return {
         --   `nvim-notify` is only needed, if you want to use the notification view.
         --   If not available, we use `mini` as the fallback
         "rcarriga/nvim-notify",
+        opts = {
+            background_colour = "#000000",
+        },
     },
     config = function()
         require("noice").setup({
